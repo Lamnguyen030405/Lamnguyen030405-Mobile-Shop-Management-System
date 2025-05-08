f272823c4ca607eb69deb219deac249e9b5a1cea
Lamnguyen030405 <nguyentrilam0304@gmail.com>
2025-05-08 15:17:29 +0700

    Last Last

--- MobileShopManagementSystem/Program.cs
+++ MobileShopManagementSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace MobileShopManagementSystem
 {
@@ -16,7 +17,29 @@ namespace MobileShopManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            InitDatabase();
             Application.Run(new Form1());
+        }
+
+        static void InitDatabase()
+        {
+            string script = System.IO.File.ReadAllText("script.txt");
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"))
+            {
+                connection.Open();
+                string[] commands = script.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string commandText in commands)
+                {
+                    string trimmed = commandText.Trim();
+                    if (!string.IsNullOrEmpty(trimmed))
+                    {
+                        using (SqlCommand command = new SqlCommand(trimmed, connection))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
         }
     }
 }
